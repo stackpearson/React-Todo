368c62b1867bf865e83dabaaf3bdc2af54e19ef0
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-07-07 22:07:03 -0600

    set up state and am mapping over it returning data

--- src/App.js
+++ src/App.js
@@ -4,11 +4,37 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor() {
+    super()
+    this.state= [
+      {
+        task: 'style my list',
+        id: 1,
+        completed: false
+      },
+      {
+        task: 'take a vacation',
+        id: 2,
+        completed: false
+      }
+    ]
+  }
   render() {
     return (
+      <>
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>To Do List</h2>
       </div>
+      
+      <div>
+        <ul>
+        {this.state.map(item => (
+          <li>{item.task}</li>
+        ))}
+        </ul>
+      </div>
+      </>
     );
   }
 }
